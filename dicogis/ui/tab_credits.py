cd0875c2b018ea4c416fbd35965b24c9fae3fb7f
Julien <dev@ingeoveritas.com>
2021-01-12 06:00:00 +0100

    Add lxml to credits

--- dicogis/ui/tab_credits.py
+++ dicogis/ui/tab_credits.py
@@ -20,6 +20,7 @@ from tkinter.ttk import Frame, Label
 
 # 3rd party
 from dxfgrabber import VERSION as dxfgrabber_version
+from lxml import __version__ as lxml_version
 from numpy import __version__ as numpy_version
 from openpyxl import __version__ as openpyxl_version
 from osgeo import __version__ as gdal_version
@@ -58,6 +59,10 @@ class TabCredits(Frame):
         self.cred_lb_gdal_name = Label(self.FrCreditsVersions, text="GDAL")
         self.cred_lb_gdal_value = Label(self.FrCreditsVersions, text=gdal_version)
 
+        # LXML
+        self.cred_lb_lxml_name = Label(self.FrCreditsVersions, text="LXML")
+        self.cred_lb_lxml_value = Label(self.FrCreditsVersions, text=lxml_version)
+
         # Numpy
         self.cred_lb_numpy_name = Label(self.FrCreditsVersions, text="Numpy")
         self.cred_lb_numpy_value = Label(self.FrCreditsVersions, text=numpy_version)
@@ -79,12 +84,14 @@ class TabCredits(Frame):
         self.cred_lb_dxfgrabber_value.grid(row=0, column=1, sticky="WE", padx=2, pady=2)
         self.cred_lb_gdal_name.grid(row=1, column=0, sticky="WE", padx=2, pady=2)
         self.cred_lb_gdal_value.grid(row=1, column=1, sticky="WE", padx=2, pady=2)
-        self.cred_lb_numpy_name.grid(row=2, column=0, sticky="WE", padx=2, pady=2)
-        self.cred_lb_numpy_value.grid(row=2, column=1, sticky="WE", padx=2, pady=2)
-        self.cred_lb_openpyxl_name.grid(row=3, column=0, sticky="WE", padx=2, pady=2)
-        self.cred_lb_openpyxl_value.grid(row=3, column=1, sticky="WE", padx=2, pady=2)
-        self.cred_lb_python_name.grid(row=4, column=0, sticky="WE", padx=3, pady=2)
-        self.cred_lb_python_value.grid(row=4, column=1, sticky="WE", padx=3, pady=2)
+        self.cred_lb_lxml_name.grid(row=2, column=0, sticky="WE", padx=2, pady=2)
+        self.cred_lb_lxml_value.grid(row=2, column=1, sticky="WE", padx=2, pady=2)
+        self.cred_lb_numpy_name.grid(row=3, column=0, sticky="WE", padx=2, pady=2)
+        self.cred_lb_numpy_value.grid(row=3, column=1, sticky="WE", padx=2, pady=2)
+        self.cred_lb_openpyxl_name.grid(row=4, column=0, sticky="WE", padx=2, pady=2)
+        self.cred_lb_openpyxl_value.grid(row=4, column=1, sticky="WE", padx=2, pady=2)
+        self.cred_lb_python_name.grid(row=5, column=0, sticky="WE", padx=3, pady=2)
+        self.cred_lb_python_value.grid(row=5, column=1, sticky="WE", padx=3, pady=2)
 
 
 # #############################################################################
