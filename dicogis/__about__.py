82e685bbdf2560c40e52f5aec0cf8e9b6bac9cf5
GeoJulien <dev@ingeoveritas.com>
2023-11-20 13:49:05 +0100

    Bump version to 3.1.1

--- dicogis/__about__.py
+++ dicogis/__about__.py
@@ -41,7 +41,7 @@ __uri_tracker__ = f"{__uri_repository__}/issues/"
 __uri__ = __uri_repository__
 
 
-__version__ = "3.1.0"
+__version__ = "3.1.1"
 __version_info__ = tuple(
     [
         int(num) if num.isdigit() else num
