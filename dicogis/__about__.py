a2dd95ab3d14694ba61c790b29a9452def211bd4
Julien M. <julien.moura@oslandia.com>
2024-05-21 15:10:29 +0200

    dev: bump version to beta 5

--- dicogis/__about__.py
+++ dicogis/__about__.py
@@ -45,7 +45,7 @@ __uri_tracker__ = f"{__uri_repository__}/issues/"
 __uri__ = __uri_repository__
 
 
-__version__ = "4.0.0-beta4"
+__version__ = "4.0.0-beta5"
 __version_info__ = tuple(
     [
         int(num) if num.isdigit() else num
