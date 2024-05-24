451caa2b059a05c6d5100e72861f27324a1b9f06
Julien M. <julien.moura@oslandia.com>
2024-05-24 13:43:22 +0200

    fix: remove refs to dxfgrabber

--- dicogis/ui/tab_credits.py
+++ dicogis/ui/tab_credits.py
@@ -19,7 +19,6 @@ from tkinter import ACTIVE, DISABLED, Tk
 from tkinter.ttk import Frame, Label
 
 # 3rd party
-from dxfgrabber import VERSION as dxfgrabber_version
 from lxml import __version__ as lxml_version
 from numpy import __version__ as numpy_version
 from openpyxl import __version__ as openpyxl_version
@@ -49,12 +48,6 @@ class TabCredits(Frame):
         self.FrCreditsVersions.grid(row=0, column=0, sticky="WE", padx=2, pady=2)
 
         # -- Widgets definition --------------------------------------------------------
-        # DXFGrabber
-        self.cred_lb_dxfgrabber_name = Label(self.FrCreditsVersions, text="DXFGrabber")
-        self.cred_lb_dxfgrabber_value = Label(
-            self.FrCreditsVersions, text=dxfgrabber_version
-        )
-
         # GDAL
         self.cred_lb_gdal_name = Label(self.FrCreditsVersions, text="GDAL")
         self.cred_lb_gdal_value = Label(self.FrCreditsVersions, text=gdal_version)
@@ -80,18 +73,16 @@ class TabCredits(Frame):
         )
 
         # -- Widgets placement ---------------------------------------------------------
-        self.cred_lb_dxfgrabber_name.grid(row=0, column=0, sticky="WE", padx=2, pady=2)
-        self.cred_lb_dxfgrabber_value.grid(row=0, column=1, sticky="WE", padx=2, pady=2)
-        self.cred_lb_gdal_name.grid(row=1, column=0, sticky="WE", padx=2, pady=2)
-        self.cred_lb_gdal_value.grid(row=1, column=1, sticky="WE", padx=2, pady=2)
-        self.cred_lb_lxml_name.grid(row=2, column=0, sticky="WE", padx=2, pady=2)
-        self.cred_lb_lxml_value.grid(row=2, column=1, sticky="WE", padx=2, pady=2)
-        self.cred_lb_numpy_name.grid(row=3, column=0, sticky="WE", padx=2, pady=2)
-        self.cred_lb_numpy_value.grid(row=3, column=1, sticky="WE", padx=2, pady=2)
-        self.cred_lb_openpyxl_name.grid(row=4, column=0, sticky="WE", padx=2, pady=2)
-        self.cred_lb_openpyxl_value.grid(row=4, column=1, sticky="WE", padx=2, pady=2)
-        self.cred_lb_python_name.grid(row=5, column=0, sticky="WE", padx=3, pady=2)
-        self.cred_lb_python_value.grid(row=5, column=1, sticky="WE", padx=3, pady=2)
+        self.cred_lb_gdal_name.grid(row=0, column=0, sticky="WE", padx=2, pady=2)
+        self.cred_lb_gdal_value.grid(row=0, column=1, sticky="WE", padx=2, pady=2)
+        self.cred_lb_lxml_name.grid(row=1, column=0, sticky="WE", padx=2, pady=2)
+        self.cred_lb_lxml_value.grid(row=1, column=1, sticky="WE", padx=2, pady=2)
+        self.cred_lb_numpy_name.grid(row=2, column=0, sticky="WE", padx=2, pady=2)
+        self.cred_lb_numpy_value.grid(row=2, column=1, sticky="WE", padx=2, pady=2)
+        self.cred_lb_openpyxl_name.grid(row=3, column=0, sticky="WE", padx=2, pady=2)
+        self.cred_lb_openpyxl_value.grid(row=3, column=1, sticky="WE", padx=2, pady=2)
+        self.cred_lb_python_name.grid(row=4, column=0, sticky="WE", padx=3, pady=2)
+        self.cred_lb_python_value.grid(row=4, column=1, sticky="WE", padx=3, pady=2)
 
 
 # #############################################################################
