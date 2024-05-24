fab23401c91ce9a31290db0fd0874887325859c3
Julien M. <julien.moura@oslandia.com>
2024-05-24 10:19:14 +0200

    fix: txt manager tests

--- tests/test_locale_loader.py
+++ tests/test_locale_loader.py
@@ -25,36 +25,60 @@ from dicogis.utils.texts import TextsManager
 class TestLocaleLoader(unittest.TestCase):
     """Test class."""
 
-    # standard methods
-    def setUp(self):
-        """Executed before each test."""
-        pass
-
-    def tearDown(self):
-        """Executed after each test."""
-        pass
-
     #  -- Tests ------------------------------------------------------------
-    def test_basic(self):
+    def test_translated_text_english(self):
         txtmngr = TextsManager(locale_folder=Path("locale"))
+        self.assertIsNone(txtmngr.language_code)
 
         # English
         en_txts = txtmngr.load_texts(dico_texts={}, language_code="EN")
         self.assertIsInstance(en_txts, dict)
+        self.assertIsInstance(txtmngr.language_code, str)
+        self.assertEqual(txtmngr.language_code, "EN")
 
+    def test_translated_text_french(self):
+        txtmngr = TextsManager(locale_folder=Path("locale"))
+        self.assertIsNone(txtmngr.language_code)
         # French
         fr_txts = txtmngr.load_texts(dico_texts={}, language_code="FR")
         self.assertIsInstance(fr_txts, dict)
+        self.assertIsInstance(txtmngr.language_code, str)
+        self.assertEqual(txtmngr.language_code, "FR")
 
+    def test_translated_text_spanish(self):
+        txtmngr = TextsManager(locale_folder=Path("locale"))
+        self.assertIsNone(txtmngr.language_code)
         # Spanish
         sp_txts = txtmngr.load_texts(dico_texts={}, language_code="ES")
         self.assertIsInstance(sp_txts, dict)
+        self.assertIsInstance(txtmngr.language_code, str)
+        self.assertEqual(txtmngr.language_code, "ES")
 
+    def test_translated_text_fallback(self):
+        txtmngr = TextsManager(locale_folder=Path("locale"))
+        self.assertIsNone(txtmngr.language_code)
         # Spanish
+        translated_txts = txtmngr.load_texts()
+        self.assertIsInstance(translated_txts, dict)
+        self.assertIsInstance(txtmngr.language_code, str)
+        self.assertEqual(txtmngr.language_code, "EN")
+
+    def test_translated_text_tuple(self):
+        txtmngr = TextsManager(locale_folder=Path("locale"))
+        self.assertIsNone(txtmngr.language_code)
+        # default
         default_txts = txtmngr.load_texts(
             dico_texts={}, language_code=locale.getlocale()
         )
         self.assertIsInstance(default_txts, dict)
+
+    def test_translated_text_isolentgh(self):
+        txtmngr = TextsManager(locale_folder=Path("locale"))
+        self.assertIsNone(txtmngr.language_code)
+
+        en_txts = txtmngr.load_texts(dico_texts={}, language_code="EN")
+        sp_txts = txtmngr.load_texts(dico_texts={}, language_code="ES")
+        fr_txts = txtmngr.load_texts(dico_texts={}, language_code="FR")
 
         # checks
         self.assertTrue(len(en_txts) == len(fr_txts) == len(sp_txts))
